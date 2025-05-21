e1caef10a5bd3539043f15839da393a46ecfebc9
Christian Karrié <ckarrie@users.noreply.github.com>
2025-05-21 11:51:05 +0200

    Create settings.py

---
+++ djcms_jks/settings.py
@@ -0,0 +1,206 @@
+import os  # isort:skip
+gettext = lambda s: s
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*x0ol8ogas(+e==^ie7e92xgj9qi5e3x4a!u4&14(_e#$4lu^7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['juliakarrieceramics.de', '127.0.0.1']
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = ['https://juliakarrieceramics.de', 'https://juliakarrieceramics.de/de/']
+CORS_ORIGIN_ALLOW_ALL = True
+
+CMS_CONFIRM_VERSION4 = True
+
+# Application definition
+ROOT_URLCONF = 'djcms_jks.urls'
+WSGI_APPLICATION = 'jkc.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'de'
+TIME_ZONE = 'Europe/Berlin'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'jkc', 'static'),
+)
+
+SITE_ID = 1
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            #os.path.join(BASE_DIR, 'jkc', 'templates'),
+            '/home/ckw/src/ckw-djangocms-juliakarrieceramics/djcms_jks/templates/'
+        ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.media',
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.tz',
+                'sekizai.context_processors.sekizai',
+                'django.template.context_processors.static',
+                'cms.context_processors.cms_settings'
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader'
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'cms.middleware.utils.ApphookReloadMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware'
+]
+
+INSTALLED_APPS = [
+    'djangocms_admin_style',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'cms',
+    'menus',
+    'sekizai',
+    'treebeard',
+    'djangocms_text_ckeditor',
+    'filer',
+    'easy_thumbnails',
+    'django_bootstrap5',
+    'djangocms_file',
+    'djangocms_icon',
+    'djangocms_link',
+    'djangocms_picture',
+    'djangocms_style',
+    'djangocms_googlemap',
+    'djangocms_video',
+    'jkc'
+]
+
+LANGUAGES = (
+    ## Customize this
+    ('de', gettext('de')),
+)
+
+LANGUAGES = (
+    ## Customize this
+    ('de', gettext('de')),
+)
+
+CMS_LANGUAGES = {
+    ## Customize this
+    1: [
+        {
+            'code': 'de',
+            'name': gettext('de'),
+            'redirect_on_fallback': True,
+            'public': True,
+            'hide_untranslated': False,
+        },
+    ],
+    'default': {
+        'redirect_on_fallback': True,
+        'public': True,
+        'hide_untranslated': False,
+    },
+}
+
+CMS_TEMPLATES = (
+    ## Customize this
+    #('jkc.html', 'JKC'),
+    ('fullwidth.html', 'Fullwidth'),
+    #('sidebar_left.html', 'Sidebar Left'),
+    #('sidebar_right.html', 'Sidebar Right')
+)
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+CMS_PERMISSION = True
+
+CMS_PLACEHOLDER_CONF = {}
+
+DATABASES = {
+    'default': {
+        'CONN_HEALTH_CHECKS': 'False',
+        'CONN_MAX_AGE': 0,
+        'DISABLE_SERVER_SIDE_CURSORS': 'False',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
+
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters'
+)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
